5ca7259f9154ffdd6c22d9779dcb5b9871486d70
bielupc <biel.altimira@estudiantat.upc.edu>
2024-06-19 10:29:58 +0200

    main layout

---
+++ src/app/components/NovaRecepta.jsx
@@ -0,0 +1,10 @@
+export default function NovaRecepta() {
+    return <div className="w-[35rem] mt-16">
+        <menu>
+            <li><button>Cancelar</button></li>
+            <li><button>Guardar</button></li>
+        </menu>
+        <form>
+        </form>
+    </div>
+}
